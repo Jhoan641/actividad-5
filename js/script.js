a91a27d063d9c2b99b2b9efffd18cc6e52c4e70d
Jhoan Fernandez <sonic43dorado@gmail.com>
2020-07-30 21:08:52 -0500

    Mi primer commit en git

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+ $(document).ready(function(){
+    $('.carousel').carousel({duration: 15,
+        indicators: true,
+        dist: 155500,
+        noWrap:true,
+        onCycleTo:null,
+        controls: true,
+        fullWidth: false,});
+  });
+      
